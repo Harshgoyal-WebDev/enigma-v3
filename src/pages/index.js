352e96352034dd79662a4808eae00165a1cfe3a8
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-10-14 11:19:14 +0530

    update-[vidushi]
    view

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,16 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import Hero from '@/components/Homepage/Hero';
+import Header from '@/components/Header';
+import About from '@/components/Home/About';
+import Whatwedo from '@/components/Home/Whatwedo';
+import Projects from '@/components/Home/Projects';
+import Services from '@/components/Home/Services';
+import Brands from '@/components/Home/Brands';
+import Testimonials from '@/components/Home/Testimonials';
+import Blogs from '@/components/Home/Blogs';
+import Faq from '@/components/Home/Faq';
+import Footer from '@/components/Footer';
 
 gsap.registerPlugin(useGSAP,ScrollTrigger)
 
@@ -10,19 +20,19 @@ export default function Home() {
   
   return (
     <>
-    {/* <Header/> */}
+    <Header/>
      <main>
     <Hero/>
-      {/* <About/>
+      <About/>
       <Whatwedo/> 
       <Projects/>
       <Services/>
       <Brands/>
       <Testimonials/>
       <Blogs/>
-      <Faq/> */}
+      <Faq/>
      </main>
-     {/* <Footer/> */}
+     <Footer/>
     </>
   );
 }
